15fefea709a8f98a3506f6f032e77472ca6fd5e3
David Rengifo <az57445@gmail.com>
2025-02-15 13:45:08 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import RegisterForm from "./modules/auth/components/registerForm.jsx";
+
+function App() {
+  return (
+    <>
+        <RegisterForm />
+    </>
+  )
+}
+
+export default App
