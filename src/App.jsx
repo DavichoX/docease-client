abdac961f1f9ddc9612f9fc5763080b5a5c7a6f2
David Rengifo <az57445@gmail.com>
2025-02-17 19:45:34 -0500

    new commit, the forms are better design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
-import RegisterForm from "./modules/auth/components/registerForm.jsx";
+import ReactDom from 'react-dom';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import RegisterForm from "./modules/auth/components/register/registerForm.jsx";
+import LoginForm from "./modules/auth/components/signIn/loginForm.jsx";
 
 function App() {
   return (
-    <>
-        <RegisterForm />
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path= "/" element={<div>HomePage</div>} />
+            <Route path="/register" element={<RegisterForm />}/>
+            <Route path="/login" element={<LoginForm />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
